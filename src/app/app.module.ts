9fc0a71466d92b6ccb38593d5e37de0e27b6f652
Diegolivia <diegolivia@hotmail.com>
2022-11-24 16:24:09 -0500

    login check

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,13 @@
 import { SharedModule } from './components/shared/shared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
-
-
-
 //Componentes
 import { LoginComponent } from './components/login/login.component';
-
-
 
 @NgModule({
   declarations: [
@@ -25,7 +19,8 @@ import { LoginComponent } from './components/login/login.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
 
   ],
   providers: [],
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -6,50 +7,60 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  form: FormGroup
-loading = false;
-  constructor(private fb: FormBuilder, private _snackBar: MatSnackBar, private router: Router) { 
+  form: FormGroup;
+  loading = false;
+  constructor(
+    private fb: FormBuilder,
+    private _snackBar: MatSnackBar,
+    private router: Router,
+    private http: HttpClient
+  ) {
     this.form = this.fb.group({
       usuario: ['', Validators.required],
-      password : ['', Validators.required],
-    })
+      password: ['', Validators.required],
+    });
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  ingresar() {
+    console.log(this.form);
+    const usuario = this.form.value.usuario;
+    const password = this.form.value.password;
+    const url = 'http://152.67.43.35:3000/api/auth/login';
+    const body = {
+      username: usuario,
+      password: password,
+    };
+    const headers = new Headers({
+      'Content-Type': 'application/json',
+    });
+    this.http
+      .post(url, body)
+      .toPromise()
+      .then((data: any) => {
+        this.fakeLoading();
+      })
+      .catch((e) => {
+        this.error();
+        this.form.reset();
+      });
   }
-ingresar(){
-  console.log(this.form)
-const usuario = this.form.value.usuario;
-const password = this.form.value.password;
 
-
-//sustituyendo el backend
-if(usuario == 'fina' && password == '123'){
-// Accedemos
-this.fakeLoading();
-}else{
-// Error Message  
-this.error();
-this.form.reset();
-}
-}
-
-error(){
+  error() {
     this._snackBar.open('Usuario o contraseña incorrectos', '', {
       duration: 5000,
       horizontalPosition: 'center',
-      verticalPosition: 'bottom'
-    })
-}
+      verticalPosition: 'bottom',
+    });
+  }
 
-fakeLoading(){
-  this.loading = true;
-  setTimeout(() => {
-    this.router.navigate(['dashboard'])
-
-  }, 1500 );
-}
+  fakeLoading() {
+    this.loading = true;
+    setTimeout(() => {
+      this.router.navigate(['dashboard']);
+    }, 1500);
+  }
 }
